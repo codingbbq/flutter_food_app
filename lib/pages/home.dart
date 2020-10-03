b964040924eac39c9b9d8b2f31bff3033a386601
Codding BBQ <codingbbq@gmail.com>
2020-10-03 17:08:08 +0530

    BottomNavigation, AppBar and Tags UI for food App

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,60 +1,108 @@
 // Food App Home Page
 
 import 'package:flutter/material.dart';
+import 'package:flutter_food_app/utils/tags.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      extendBody: true,
+      appBar: AppBar(
+        shadowColor: Colors.transparent,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.menu),
+          color: Colors.black,
+          iconSize: 30.0,
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+            color: Colors.black,
+            iconSize: 30.0,
+          )
+        ],
+      ),
       bottomNavigationBar: Container(
-        child: BottomNavigationBar(
-          showSelectedLabels: false,
-          showUnselectedLabels: false,
-          elevation: 20.0,
-          backgroundColor: Colors.black,
-          iconSize: 30,
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.home,
-                  semanticLabel: "Home",
-                ),
-                title: Text("Home")),
-            BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.account_balance_wallet,
-                  semanticLabel: "Balance",
-                ),
-                title: Text("Balance")),
-            BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.chat,
-                  semanticLabel: "Chat",
-                ),
-                title: Text("Chat")),
-            BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.person,
-                  semanticLabel: "Profile",
-                ),
-                title: Text("Profile"))
-          ],
+        padding: EdgeInsets.symmetric(
+          horizontal: 20.0,
+          vertical: 10.0,
+        ),
+        color: Colors.white,
+        child: ClipRRect(
+          clipBehavior: Clip.hardEdge,
+          borderRadius: BorderRadius.all(Radius.circular(30.0)),
+          child: BottomNavigationBar(
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            iconSize: 34,
+            selectedItemColor: Colors.white,
+            unselectedItemColor: Colors.white,
+            backgroundColor: Colors.black,
+            type: BottomNavigationBarType.fixed,
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.home,
+                    semanticLabel: "Home",
+                  ),
+                  title: Text("Home")),
+              BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.account_balance_wallet,
+                    semanticLabel: "Balance",
+                  ),
+                  title: Text("Balance")),
+              BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.chat,
+                    semanticLabel: "Chat",
+                  ),
+                  title: Text("Chat")),
+              BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.person,
+                    semanticLabel: "Profile",
+                  ),
+                  title: Text("Profile"))
+            ],
+          ),
         ),
       ),
       body: SafeArea(
         child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 15.0),
           color: Colors.white,
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              // For the Navigation
-              Placeholder(fallbackHeight: 50),
               SizedBox(height: 10.0),
               // For Title and Subtitle
-              Placeholder(fallbackHeight: 60),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Delicious Salads",
+                    style: TextStyle(
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    "We make fresh and healthy food",
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  )
+                ],
+              ),
               SizedBox(height: 10.0),
               // For Tags
-              Placeholder(fallbackHeight: 80, color: Colors.grey),
+              Tags(),
+
               SizedBox(
                 height: 10.0,
               ),
---
+++ lib/utils/tags.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class Tags extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 10.0),
+      child: Row(
+        children: [
+          // Selected Tag
+          Container(
+            padding: EdgeInsets.symmetric(
+              horizontal: 15.0,
+              vertical: 10.0,
+            ),
+            margin: EdgeInsets.only(right: 20.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30.0),
+              color: Colors.black,
+            ),
+            child: Text(
+              "Salaads",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18.0,
+              ),
+            ),
+          ),
+
+          // Not selected tag
+          Container(
+            padding: EdgeInsets.symmetric(
+              horizontal: 15.0,
+              vertical: 10.0,
+            ),
+            margin: EdgeInsets.only(right: 20.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30.0),
+              color: Colors.grey[300],
+            ),
+            child: Text(
+              "Soups",
+              style: TextStyle(
+                color: Colors.grey[800],
+                fontSize: 18.0,
+              ),
+            ),
+          ),
+
+          // Not selected tag
+          Container(
+            padding: EdgeInsets.symmetric(
+              horizontal: 15.0,
+              vertical: 10.0,
+            ),
+            margin: EdgeInsets.only(right: 20.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30.0),
+              color: Colors.grey[300],
+            ),
+            child: Text(
+              "Grilled",
+              style: TextStyle(
+                color: Colors.grey[800],
+                fontSize: 18.0,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
